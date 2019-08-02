6cef42db86cddd0bb5d8e95493af117631fec5e2
周梦梦 <zhoumengmeng@souche.com>
2019-08-02 10:55:45 +0800

    feat: get path简单支持.

--- get.js
+++ get.js
@@ -2,23 +2,29 @@
  *
  *
  * @param {Object} object
- * @param {Array} path 
- * @param {*} [defaultValue='undefined']
+ * @param {Array|string} path string只支持.,eg: a.b.c
+ * @param {*} defaultValue
  * @returns {*} Returns the resolved value.
  */
-function get(object, path, defaultValue = 'undefined') {
-    const type = typeof object
-    
-    if (object === null && type !== 'object') {
-      return defaultValue
+function get(object, path, defaultValue) {
+    const result = (object !== null && typeof object === 'object') ? undefined : baseGet(obj, path)
+    return result === undefined ? defaultValue : result
+}
+
+function baseGet(object, path) {
+    const pathArr = []
+    if (Array.isArray(path)) {
+        pathArr = path
+    } else {
+        pathArr = path.split('.')
     }
-    
+
     let index = 0
-    const length = path.length
+    const length = pathArr.length
     while(index < length && object) {
-      object = object[path[index++]]
+      object = object[pathArr[index++]]
     }
-    return (index && object) ? object : defaultValue
+    return (index && index === length) ? object : undefined
 }
 
 export default get
